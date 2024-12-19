3ecfa365cc082040ad2c6607ed3d6373459b4fc5
MichaelLELU <lelu.michael@gmail.com>
2024-12-19 16:40:51 +0100

    make hotRelod worck and css issues

--- frontend/src/organisms/AdCreationForm.tsx
+++ frontend/src/organisms/AdCreationForm.tsx
@@ -2,92 +2,93 @@ import { useEffect, type FormEvent } from "react";
 import Select from "react-select";
 import { useNavigate } from "react-router-dom";
 import {
-	type AdInput,
-	useCreateAdMutation,
-	useGetCategoriesAndTagsQuery,
+  type AdInput,
+  useCreateAdMutation,
+  useGetCategoriesAndTagsQuery,
 } from "../libs/graphql/generated/graphql-types";
 
 export default function AdCreationForm() {
-	const { loading, error, data } = useGetCategoriesAndTagsQuery();
-	const [createAd, { data: dataSub, loading: subLoading, error: subError }] =
-		useCreateAdMutation();
+  const { loading, error, data } = useGetCategoriesAndTagsQuery();
+  const [createAd, { data: dataSub, loading: subLoading, error: subError }] =
+    useCreateAdMutation();
 
-	const navigate = useNavigate();
+  const navigate = useNavigate();
 
-	const hSubmit = (evt: FormEvent) => {
-		evt.preventDefault();
+  const hSubmit = (evt: FormEvent) => {
+    evt.preventDefault();
 
-		const form = evt.target;
-		const formData = new FormData(form as HTMLFormElement);
-		const formJson = Object.fromEntries(formData.entries());
+    const form = evt.target;
+    const formData = new FormData(form as HTMLFormElement);
+    const formJson = Object.fromEntries(formData.entries());
 
-		const formattedData = {
-			...formJson,
-			price: Number.parseFloat(formJson.price as string),
-			tags: (formJson.tags as string).split(","),
-		};
+    const formattedData = {
+      ...formJson,
+      price: Number.parseFloat(formJson.price as string),
+      tags: (formJson.tags as string).split(","),
+    };
 
-		createAd({ variables: { data: formattedData as AdInput } });
-	};
+    createAd({ variables: { data: formattedData as AdInput } });
+  };
 
-	useEffect(() => {
-		if (!dataSub) return;
-		navigate(`/ads/${dataSub.createAd.id}`);
-	}, [dataSub, navigate]);
+  useEffect(() => {
+    if (!dataSub) return;
+    navigate(`/ads/${dataSub.createAd.id}`);
+  }, [dataSub, navigate]);
 
-	if (error || subError) return <>Error!</>;
-	if (loading) return <>Loading...</>;
-	if (!data) return <>We couldn't find anything to display</>;
-	return (
-		<main className="main-content">
-			<form onSubmit={hSubmit}>
-				<label>
-					Titre:
-					<input className="text-field" name="title" />
-				</label>
-				<label>
-					Description:
-					<input className="text-field" name="description" />
-				</label>
-				<label>
-					Owner:
-					<input className="text-field" name="owner" />
-				</label>
-				<label>
-					Price:
-					<input className="text-field" name="price" type="number" />
-				</label>
-				<label>
-					Picture:
-					<input className="text-field" name="picture" />
-				</label>
-				<label>
-					Location:
-					<input className="text-field" name="location" />
-				</label>
-				<select name="category">
-					{data.getCategories.map((category) => (
-						<option key={category.id} value={category.id}>
-							{category.name}
-						</option>
-					))}
-				</select>
-				<label>
-					Tags:
-					<Select
-						options={data.getTags.map((apiTag: ApiTag) => ({
-							value: apiTag.id,
-							label: apiTag.name,
-						}))}
-						isMulti
-						name="tags"
-						delimiter=","
-					/>
-				</label>
-				<button type="submit" className="button" disabled={subLoading}>
-					Create Ad!
-				</button>
-			</form>
-		</main>
-	);
+  if (error || subError) return <>Error!</>;
+  if (loading) return <>Loading...</>;
+  if (!data) return <>We couldn't find anything to display</>;
+  return (
+    <main className="main-content">
+      <form onSubmit={hSubmit}>
+        <label>
+          Titre:
+          <input className="text-field" name="title" />
+        </label>
+        <label>
+          Description:
+          <input className="text-field" name="description" />
+        </label>
+        <label>
+          Owner:
+          <input className="text-field" name="owner" />
+        </label>
+        <label>
+          Price:
+          <input className="text-field" name="price" type="number" />
+        </label>
+        <label>
+          Picture:
+          <input className="text-field" name="picture" />
+        </label>
+        <label>
+          Location:
+          <input className="text-field" name="location" />
+        </label>
+        <select name="category">
+          <option value="-">Categories</option>
+          {data.getCategories.map((category) => (
+            <option key={category.id} value={category.id}>
+              {category.name}
+            </option>
+          ))}
+        </select>
+        <label>
+          Tags:
+          <Select
+            options={data.getTags.map((apiTag: ApiTag) => ({
+              value: apiTag.id,
+              label: apiTag.name,
+            }))}
+            isMulti
+            name="tags"
+            delimiter=","
+          />
+        </label>
+        <button type="submit" className="button" disabled={subLoading}>
+          Create Ad!
+        </button>
+      </form>
+    </main>
+  );
 }
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
-	server: { host: true },
+  plugins: [react()],
+  server: { host: true, hmr: { path: "hmr" } },
 });
